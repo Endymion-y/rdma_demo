a9b405c8403ef87e0786a6da6a33e02bb3239955
Endymion <495609715@qq.com>
2017-03-20 12:46:35 +0800

    commit

--- client.cpp
+++ client.cpp
@@ -1,35 +1,55 @@
 // RDMA client
 #include <iostream>
 #include <cstdio>
+#include <cstdlib>
+#include <netdb.h>
+#include <memory>
 
 #include <rdma/rdma_cma.h>
 #include <rdma/rdma_verbs.h>
+
+const int MSGSIZ = 128;
 
 int main(int argc, char* argv[]){
 	int ret;
 
 	// --------------- Setup Phase ---------------
 	// Obtain and convert addressing information
-	char* node = argv[1];        // IP
-	char* service = argv[2];     // Port
-	struct rdma_addrinfo hints = {};
+	char* IP = argv[1];        
+	char* port = argv[2];     
+	struct rdma_addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_port_space = RDMA_PS_TCP;
 	struct rdma_addrinfo* res;
-	ret = rdma_getaddrinfo(node, service, &hints, &res);
+	if ((ret = rdma_getaddrinfo(IP, port, &hints, &res)))
+		perror("rdma_getaddrinfo");
+	assert(res);
 
 	// Create and configure local endpoints for communication
-	// struct ibv_qp_init_attr qp_init_attr;
+	struct ibv_qp_init_attr qp_init_attr;
 	struct rdma_cm_id* id;
 	// struct ibv_pd pd;
-	ret = rdma_create_ep(&id, res, NULL, NULL);
+	memset(&qp_init_attr, 0, sizeof(qp_init_attr));
+	qp_init_attr.cap.max_send_wr = 1;
+	qp_init_attr.cap.max_recv_wr = 1;
+	qp_init_attr.cap.max_send_sge = 1;
+	qp_init_attr.cap.max_recv_sge = 1;
+	qp_init_attr.cap.max_inline_data = MSGSIZ;
+	qp_init_attr.qp_context = id;
+	qp_init_attr.sq_sig_all = true;
+	if ((ret = rdma_create_ep(&id, res, NULL, &qp_init_attr)))
+		perror("rdma_create_ep");
+	assert(id);
 
 	// Setup local memory to be used in transfer
-	int MSGSIZ = 1024;
 	void* msg = malloc(MSGSIZ);
 	struct ibv_mr* mr = rdma_reg_msgs(id, msg, MSGSIZ);
+	assert(mr);
 
 	// Establish the connection with the remote side
-	struct rdma_conn_param conn_param;
-	ret = rdma_connect(id, &conn_param);
+	// struct rdma_conn_param conn_param;
+	if ((ret = rdma_connect(id, NULL)))
+		perror("rdma_connect");
 
 	// --------------- Use Phase ---------------
 	// Actually transfer data to/from the remote side
@@ -37,18 +57,25 @@ int main(int argc, char* argv[]){
 	sprintf((char*)msg, "Hello, world");
 	printf("Message sent: %s\n", (char*)msg);
 
-	ret = rdma_post_send(id, NULL, msg, MSGSIZ, mr, 0);
-	while ((ret = ibv_poll_cq(id->send_cq, 1, &wc)) <= 0)
+	if ((ret = rdma_post_send(id, NULL, msg, MSGSIZ, mr, 0)))
+		perror("rdma_post_send");
+	while ((ret = ibv_poll_cq(id->send_cq, 1, &wc)) == 0)
 		/* Waiting */ ;
-	ret = rdma_post_recv(id, NULL, msg, MSGSIZ, mr);
-	while ((ret = ibv_poll_cq(id->recv_cq, 1, &wc)) <= 0)
+	if (ret < 0) perror("ibv_poll_cq");
+	if ((ret = rdma_post_recv(id, NULL, msg, MSGSIZ, mr)))
+		perror("rdma_post_recv");
+	while ((ret = ibv_poll_cq(id->recv_cq, 1, &wc)) == 0)
 		/* Waiting */ ;
+	if (ret < 0) perror("ibv_poll_cq");
 	printf("Message received: %s\n", (char*)msg);
 
 	// ------------ Break-down Phase ------------
 	// Close connection, free memory and communication resources
-	ret = rdma_disconnect(id);
-	ret = rdma_dereg_mr(mr);
+	if ((ret = rdma_disconnect(id)))
+		perror("rdma_disconnect");
+	if ((ret = rdma_dereg_mr(mr)))
+		perror("rdma_dereg_mr");
+	free(msg);
 	rdma_freeaddrinfo(res);
 
 	return 0;
--- server.cpp
+++ server.cpp
@@ -2,26 +2,29 @@
 #include <iostream>
 #include <memory>
 #include <cstdio>
+#include <cstdlib>
+#include <netdb.h>
 
 #include <rdma/rdma_cma.h>
 #include <rdma/rdma_verbs.h>
 
-const int MSGSIZ = 1024;
+const int MSGSIZ = 128;
 
 int main(int argc, char* argv[]){
 	int ret;
 
 	// --------------- Setup Phase ---------------
 	// Obtain and convert addressing information
-	char* node = NULL;
-	char* service = argv[1];
+	char* IP = NULL;
+	char* port = argv[1];
 	struct rdma_addrinfo hints;
 	struct rdma_addrinfo* res;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_flags = RAI_PASSIVE;          // Server
 	hints.ai_port_space = RDMA_PS_TCP;
-	ret = rdma_getaddrinfo(node, service, &hints, &res);
+	if ((ret = rdma_getaddrinfo(IP, port, &hints, &res)))
+		perror("rdma_getaddrinfo");
 
 	// Create and configure local endpoints for communication
 	struct ibv_qp_init_attr qp_init_attr;
@@ -35,53 +38,66 @@ int main(int argc, char* argv[]){
 	qp_init_attr.cap.max_recv_sge = 1;
 	qp_init_attr.cap.max_inline_data = MSGSIZ;
 	qp_init_attr.sq_sig_all = 1;       // Whether send generates CQE
-	ret = rdma_create_ep(&id, res, NULL, &qp_init_attr);
+	if ((ret = rdma_create_ep(&id, res, NULL, &qp_init_attr)))
+		perror("rdma_create_ep");
 
 	// Establish socket as listener
-	ret = rdma_listen(id, 0);
+	if ((ret = rdma_listen(id, 0)))
+		perror("rdma_listen");
 
 	// Loop
 	while (true){
 		struct rdma_cm_id* conn_id;
 		// Get connection request from client
-		rdma_get_request(id, &conn_id);
+		if ((ret = rdma_get_request(id, &conn_id)))
+			perror("rdma_get_request");
 
 		// ------------- Agent Setup -------------
 		// Allocate memory and register
 		void* msg = malloc(MSGSIZ);
 		struct ibv_mr* mr = rdma_reg_msgs(conn_id, msg, MSGSIZ);
+		assert(mr);
 		// Define properties of new connection
-		struct rdma_conn_param conn_param;
+		// struct rdma_conn_param conn_param;
 		// Finalize connection with client
-		rdma_accept(conn_id, NULL);
+		if ((ret = rdma_accept(conn_id, NULL)))
+			perror("rdma_accept");
 
 		// ------------- Agent Use ---------------
 		struct ibv_wc wc;      // CQE array with length 1
 		// Post receive
-		ret = rdma_post_recv(conn_id, NULL, msg, MSGSIZ, mr);
+		if ((ret = rdma_post_recv(conn_id, NULL, msg, MSGSIZ, mr)))
+			perror("rdma_post_recv");
 		// Wait to receive ping data
-		while ((ret = ibv_poll_cq(conn_id->recv_cq, 1, &wc)) <= 0)
+		while ((ret = ibv_poll_cq(conn_id->recv_cq, 1, &wc)) == 0)
 			/* Waiting */ ;
+		if (ret < 0) perror("ibv_poll_cq");
 		// Post send
-		ret = rdma_post_send(conn_id, NULL, msg, MSGSIZ, mr, 0);
+		if ((ret = rdma_post_send(conn_id, NULL, msg, MSGSIZ, mr, 0)))
+			perror("rdma_post_send");
 		// Wait for send to complete
-		while ((ret = ibv_poll_cq(conn_id->send_cq, 1, &wc)) <= 0)
+		while ((ret = ibv_poll_cq(conn_id->send_cq, 1, &wc)) == 0)
 			/* Waiting */ ;
+		if (ret < 0) perror("ibv_poll_cq");
 
 		// -------- Agent Break-down -------------
 		// Break connection
-		rdma_disconnect(conn_id);
+		if ((ret = rdma_disconnect(conn_id)))
+			perror("rdma_disconnect");
 		// Deregister user virtual memory
-		rdma_dereg_mr(mr);
+		if ((ret = rdma_dereg_mr(mr)))
+			perror("rdma_dereg_mr");
 		// Free memory
 		free(msg);
 		// Destroy local end point
-		rdma_destroy_ep(conn_id);
+		if ((ret = rdma_destroy_ep(conn_id)))
+			perror("rdma_destroy_ep");
 	}
 
 	// ------------ Break-down Phase ------------
 	// Close connection, free memory and communication resources
-	ret = rdma_disconnect(id);
+	if ((ret = rdma_disconnect(id)))
+		perror("rdma_disconnect");
 	rdma_freeaddrinfo(res);
 
 	return 0;
